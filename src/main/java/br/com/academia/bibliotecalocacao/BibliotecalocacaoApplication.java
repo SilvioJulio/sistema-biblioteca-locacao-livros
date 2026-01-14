20887132d67c7c3be52d34fb493114587077ebc8
Silvio Julio <sivicr3@hotmail.com>
2026-01-14 17:56:50 -0300

    commit projeto biblioteca locação

---
+++ src/main/java/br/com/academia/bibliotecalocacao/BibliotecalocacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.academia.bibliotecalocacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecalocacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecalocacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/academia/bibliotecalocacao/BibliotecalocacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academia.bibliotecalocacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecalocacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
