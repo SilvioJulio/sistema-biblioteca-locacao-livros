4f1fd6f397c6e5bd0099bc206de168edd5bf3aa2
Silvio Julio <sivicr3@hotmail.com>
2026-01-15 17:05:33 -0300

    feat: adiciona entidade Autor e interface AutorRepository

---
+++ src/main/java/br/com/academia/bibliotecalocacao/entity/Autor.java
@@ -0,0 +1,13 @@
+package br.com.academia.bibliotecalocacao.entity;
+
+import java.time.LocalDate;
+
+public class Autor {
+
+    private Long id;
+    private String nome;
+    private String sexo;
+    private LocalDate anoNascimento;
+    private String cpf;
+
+}
--- src/main/java/br/com/academia/bibliotecalocacao/repository/AutorRepository.java
+++ src/main/java/br/com/academia/bibliotecalocacao/repository/AutorRepository.java
@@ -1,4 +1,11 @@
 package br.com.academia.bibliotecalocacao.repository;
 
-public interface AutorRepository {
+import br.com.academia.bibliotecalocacao.entity.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AutorRepository extends JpaRepository <Autor,Long>{
+            List<Autor> findByNome(String nome);
+           List<Autor> findByNomeContainingIgnoreCase(String nome);
 }
