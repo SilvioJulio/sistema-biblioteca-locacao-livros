80ac9a466fc07f150d0f1073e5925280f2c019b4
Silvio Julio <sivicr3@hotmail.com>
2026-01-21 10:05:56 -0300

    feat: implementa LocatarioNotFoundException e testes de integração de aluguel

--- src/main/java/br/com/academia/bibliotecalocacao/exception/LocatarioNotFoundException.java
+++ src/main/java/br/com/academia/bibliotecalocacao/exception/LocatarioNotFoundException.java
@@ -1,8 +1,11 @@
 package br.com.academia.bibliotecalocacao.exception;
 
-
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class LocatarioNotFoundException extends ChangeSetPersister.NotFoundException {
     private final String message;
 
